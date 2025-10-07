0d36682acb384bbb0458567787f29f942e955337
Daniel1234Daniel <dtekleyohannes@gmail.com>
2025-10-07 10:49:47 +0300

    route guard testing ...

--- src/lib/roleRoutes.ts
+++ src/lib/roleRoutes.ts
@@ -48,34 +48,58 @@ export const allRoutes: RouteItem[] = [
       roles: ["super_admin"]
     },
 
-    { href: "/dashboard/superadmin/admins", label: "Admins", icon: Users ,},
-    { href: "/dashboard/superadmin/role", label: "Roles", icon: ShieldCheck },
-    { href: "/dashboard/superadmin/plan", label: "Plans", icon: Banknote },
+    { href: "/dashboard/superadmin/admins", 
+      label: "Admins",
+       icon: Users ,
+      permission: "",
+      roles: ["super_admin"]
+      },
+    { href: "/dashboard/superadmin/role", 
+      label: "Roles",
+       icon: ShieldCheck,
+            permission: "create_role",
+      roles: ["super_admin"] },
+    {
+       href: "/dashboard/superadmin/plan",
+       label: "Plans", 
+       icon: Banknote,
+            permission: "",
+      roles: ["super_admin"] },
     {
       href: "/dashboard/superadmin/subscription",
       label: "Subscriptions",
       icon: BadgeDollarSign,
+            permission: "",
+      roles: ["super_admin"]
     },
     {
       href: "/dashboard/superadmin/support-tickets",
       label: "Support Tickets",
       icon: Ticket,
+            permission: "",
+      roles: ["super_admin"]
     },
     {
       href: "/dashboard/superadmin/videos",
       label: "Media Management",
       icon: Video,
+            permission: "",
+      roles: ["super_admin"]
     },
     {
       href: "/dashboard/superadmin/activity-log",
       label: "Activity Logs",
       icon: FileClock,
+            permission: "",
+      roles: ["super_admin"]
     },
 
     {
       href: "/dashboard/superadmin/settings",
       label: "Settings",
       icon: Settings,
+            permission: "",
+      roles: ["super_admin"]
     },
     {
        href: "/dashboard/superadmin/admins", 
@@ -132,23 +156,79 @@ export const allRoutes: RouteItem[] = [
       permission: "",
       roles: ["restaurant_admin"]
     },
-    { href: "/dashboard/restaurant/role", label: "Role", icon: UserCog },
-    { href: "/dashboard/restaurant/staff", label: "staff", icon: UserCog },
-    { href: "/dashboard/restaurant/branch", label: "branch", icon: Store },
-    { href: "/dashboard/restaurant/menu", label: "Menu", icon: ListOrdered },
-    { href: "/dashboard/restaurant/menu-categories", label: "Menu Categories", icon: Folder },
-    { href: "/dashboard/restaurant/menu-items", label: "Menu Items", icon: ListOrdered },
-    { href: "/dashboard/restaurant/tables", label: "Tables", icon: Utensils },
-    { href: "/orders", label: "Orders", icon: ShoppingBasket },
-    { href: "/dashboard/restaurant/catering", label: "Catering", icon: Utensils },
-    { href: "/dashboard/restaurant/catering-requests", label: "Catering Requests", icon: Utensils },
-    { href: "/dashboard/restaurant/accepted-catering-requests", label: "Accepted Catering Requests", icon: Utensils },
-    { href: "/dashboard/restaurant/social-media", label: "Social Media", icon: Camera },
-    { href: "/dashboard/restaurant/subscription", label: "Subscription", icon: CreditCard },
-    { href: "/dashboard/restaurant/transactions", label: "Transactions", icon: DollarSign },
-    { href: "/dashboard/restaurant/activity-logs", label: "Activity Logs", icon: Logs },
-    { href: "/dashboard/restaurant/support-ticket", label: "Support Ticket", icon: Ticket },
-    { href: "/dashboard/restaurant/settings", label: "Settings", icon: Settings },
+    { href: "/dashboard/restaurant/role", 
+      label: "Role", 
+      icon: UserCog,
+      permission: "",
+      roles: ["restaurant_admin"]
+     },
+    { href: "/dashboard/restaurant/staff",
+       label: "staff", icon: UserCog,
+          permission: "",
+      roles: ["restaurant_admin"] },
+    { href: "/dashboard/restaurant/branch", label: "branch", icon: Store,
+          permission: "",
+      roles: ["restaurant_admin"]
+     },
+    { href: "/dashboard/restaurant/menu", label: "Menu", icon: ListOrdered,
+          permission: "",
+      roles: ["restaurant_admin"]
+     },
+    { href: "/dashboard/restaurant/menu-categories", 
+      label: "Menu Categories",
+       icon: Folder,
+      permission: "",
+      roles: ["restaurant_admin"]
+     },
+    { href: "/dashboard/restaurant/menu-items", label: "Menu Items", icon: ListOrdered,
+          permission: "",
+      roles: ["restaurant_admin"]
+     },
+    { href: "/dashboard/restaurant/tables", label: "Tables", icon: Utensils,
+          permission: "",
+      roles: ["restaurant_admin"]
+     },
+    { href: "/dashboard/restaurant/orders", label: "Orders", icon: ShoppingBasket,
+          permission: "",
+      roles: ["restaurant_admin"]
+     },
+    { href: "/dashboard/restaurant/catering", label: "Catering", icon: Utensils,
+          permission: "",
+      roles: ["restaurant_admin"]
+     },
+    { href: "/dashboard/restaurant/catering-requests", label: "Catering Requests", icon: Utensils,
+          permission: "",
+      roles: ["restaurant_admin"]
+     },
+    { href: "/dashboard/restaurant/accepted-catering-requests", label: "Accepted Catering Requests", icon: Utensils,
+          permission: "",
+      roles: ["restaurant_admin"]
+     },
+    { href: "/dashboard/restaurant/social-media", label: "Social Media", icon: Camera,
+          permission: "",
+      roles: ["restaurant_admin"]
+     },
+    { href: "/dashboard/restaurant/subscription", label: "Subscription", icon: CreditCard,
+          permission: "",
+      roles: ["restaurant_admin"]
+     },
+    { href: "/dashboard/restaurant/transactions", label: "Transactions", icon: DollarSign,
+          permission: "",
+      roles: ["restaurant_admin"]
+     },
+    { href: "/dashboard/restaurant/activity-logs", label: "Activity Logs", icon: Logs ,
+          permission: "",
+      roles: ["restaurant_admin"]
+    },
+    { href: "/dashboard/restaurant/support-ticket", label: "Support Ticket", icon: Ticket,
+          permission: "",
+      roles: ["restaurant_admin"]
+     },
+    { href: "/dashboard/restaurant/settings", 
+      label: "Settings", icon: Settings,
+          permission: "",
+      roles: ["restaurant_admin"]
+     },
 
 
     { 
@@ -183,11 +263,12 @@ export const hasPermission = (user: any, permission: string): boolean => {
 
 export const filterRoutesByPermission = (routes: RouteItem[], user: any): RouteItem[] => {
 
-  
+     console.log(user,'user permissions')
+    
 
   return routes.filter(route => {
         
-    if (route.roles && !route.roles.includes(user.role_tag)) {
+    if ((route.roles?.length !== 0) && route.roles && !route.roles.includes(user.role_tag)) {
       return false;
     }
     
